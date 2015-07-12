31181b75e090be3b95536a1976b9d645933d94c9
Bill Jordan <bill@billjordan.info>
2015-07-12 12:56:18 -0400

    mark responses as from api

--- app.py
+++ app.py
@@ -20,22 +20,24 @@ def hello_world():
 def get_categories():
     # categories = ["Category 1", "Category 2", "Category 3"]
     # return list of dicts ensure order by sorting
-    return json.dumps(categories, default=obj_dict, sort_keys=True)
+    cats = {"categories": categories}
+    return json.dumps(cats, default=obj_dict, sort_keys=True)
 
 @app.route('/facts/<int:category_id>')
 def get_facts(category_id):
     # facts = ["Fact 1", "Fact 2", "Fact 3"]
     # return list of dicts ensure order by sorting
-    return json.dumps(facts, default=obj_dict, sort_keys=True)
+    facts_dict = {"facts": facts}
+    return json.dumps(facts_dict, default=obj_dict, sort_keys=True)
 
 if __name__ == '__main__':
-    category1 = Category("category 1", 1)
-    category2 = Category("category 2", 2)
-    category3 = Category("category 3", 3)
+    category1 = Category("API category 1", 1)
+    category2 = Category("API category 2", 2)
+    category3 = Category("API category 3", 3)
     categories = [category1, category2, category3]
-    fact1 = Fact("Fact 1", 1)
-    fact2 = Fact("Fact 2", 2)
-    fact3 = Fact("Fact 3", 3)
+    fact1 = Fact("API Fact 1", 1)
+    fact2 = Fact("API Fact 2", 2)
+    fact3 = Fact("API Fact 3", 3)
     facts = [fact1, fact2, fact3]
 
 
